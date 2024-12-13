b290b27ca2a5aa7953f14c8aff97be25bb3112ea
gp-kato <kato@gp1000.jp>
2024-12-13 15:10:51 +0900

    初期ファイル作成

---
+++ src/laravel-chat/resources/views/chat.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'GroupChat')
+
+@section('content')
+    
+@endsection
---
+++ src/laravel-chat/resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <header id="header" class="wrapper">
+            <div class="site-title">
+            </div>
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer id="footer" class="wrapper">
+        </footer>
+    </body>
+</html>
