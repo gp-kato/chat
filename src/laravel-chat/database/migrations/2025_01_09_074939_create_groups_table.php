18fe7b9c655e953f979600d6c7984b3450ff4483
gp-kato <kato@gp1000.jp>
2025-01-15 14:19:41 +0900

    [fix] スペルミスを修正

--- src/laravel-chat/database/migrations/2025_01_09_074939_create_groups_table.php
+++ src/laravel-chat/database/migrations/2025_01_09_074939_create_groups_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('desdription');
+            $table->text('description');
             $table->timestamps();
         });
     }
