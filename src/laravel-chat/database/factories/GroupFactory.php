8e0fa374816942991763cc7a4377b6b8fe4b5099
gp-kato <kato@gp1000.jp>
2025-01-21 13:30:09 +0900

    [feat] シーダーとファクトリーを作成

---
+++ src/laravel-chat/database/factories/GroupFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    protected $model = \App\Models\Group::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+}
