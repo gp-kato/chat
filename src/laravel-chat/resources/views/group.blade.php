76c16c3bae71e176bbad4ce1f1ca3ea49a4973fc
gp-kato <kato@gp1000.jp>
2025-02-18 14:11:06 +0900

    [fix] 誤ったパス生成メソッドを修正

--- src/laravel-chat/resources/views/group.blade.php
+++ src/laravel-chat/resources/views/group.blade.php
@@ -5,21 +5,12 @@
 @section('content')
     <div class="container">
         <h1>Group Chat</h1>
-        <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-        <div x-data="{ show: false }">
-            <h2 @click="show = !show" style="cursor: pointer;">ユーザー</h2>
-            <ul x-show="show">
-                @foreach($users as $user)
-                    <li>{{ $user->name }}</li>
-                @endforeach
-            </ul>
-        </div>
         <h2>チャット</h2>
         <div id="messages" class="chat-list">
             <ul>
                 @foreach($groups as $group)
                     <li>
-                        <a href="{{ url('group/' . $group->id) }}">{{ $group->name }}</a>
+                        <a href="{{ route('show', [$group->id]) }}">{{ $group->name }}</a>
                     </li>
                 @endforeach
             </ul>
