93fd264f7e5d9f03380cd99626e8daa90b337382
gp-kato <kato@gp1000.jp>
2025-02-20 13:40:00 +0900

    [fix] cssの移転のし直し

--- src/laravel-chat/resources/views/layout.blade.php
+++ src/laravel-chat/resources/views/layout.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
         <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-        <link rel="stylesheet" href="/css/style.css">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
