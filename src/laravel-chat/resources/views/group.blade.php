06af94c3fa28b75e4c1fbb2463ffba377d868439
gp-kato <kato@gp1000.jp>
2025-02-28 13:42:11 +0900

    [fix] バリデーションエラー時にエラーメッセージを表示

--- src/laravel-chat/resources/views/group.blade.php
+++ src/laravel-chat/resources/views/group.blade.php
@@ -15,6 +15,15 @@
                 @endforeach
             </ul>
         </div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action="{{ route('add') }}" method="POST">
             @csrf
             <input type="text" name="name">
