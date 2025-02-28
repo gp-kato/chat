e4e93973fe7be68b7c966b33299be36cca7af31f
gp-kato <kato@gp1000.jp>
2025-02-28 14:09:47 +0900

    [fix] oldでバリデーションエラー時に入力値を保持

--- src/laravel-chat/resources/views/group.blade.php
+++ src/laravel-chat/resources/views/group.blade.php
@@ -27,9 +27,9 @@
         <form action="{{ route('add') }}" method="POST">
             @csrf
             <label for="name">チャット名</label>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old('name') }}">
             <label for="description">説明文</label>
-            <input type="text" name="description">
+            <input type="text" name="description" value="{{ old('description') }}">
             <button type="submit" class="btn btn-primary">新チャット作成</button>
         </form>
     </div>
