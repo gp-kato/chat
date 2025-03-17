642bf1759663273342283d82655aeb961fb00b91
gp-kato <kato@gp1000.jp>
2025-03-17 16:39:09 +0900

    [fix] ナビゲーション全体を流用

--- src/laravel-chat/resources/views/layout.blade.php
+++ src/laravel-chat/resources/views/layout.blade.php
@@ -9,14 +9,7 @@
     </head>
     <body>
         <header id="header" class="wrapper">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <x-dropdown-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                    this.closest('form').submit();">
-                    {{ __('Log Out') }}
-                </x-dropdown-link>
-            </form>
+            @include('layouts.navigation')
         </header>
 
         <main>
