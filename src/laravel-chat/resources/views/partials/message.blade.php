d7c7e6e5e7b865051e656042c205ea0557440de8
gp-kato <kato@gp1000.jp>
2025-10-17 13:55:57 +0900

    [feat] message.bladeをli タグから記述

--- src/laravel-chat/resources/views/partials/message.blade.php
+++ src/laravel-chat/resources/views/partials/message.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <body>
-        <li class="d-flex {{ $message->user->id === auth()->id() ? 'justify-content-end' : 'justify-content-start' }}">
-            <div>
-                {!! nl2br(e($message->content)) !!}
-                <br>
-                <small>by {{ $message->user->name }}</small>
-            </div>
-        </li>
-    </body>
-</html>
+<li class="d-flex {{ $message->user->id === auth()->id() ? 'justify-content-end' : 'justify-content-start' }}">
+    <div>
+        {!! nl2br(e($message->content)) !!}
+        <br>
+        <small>by {{ $message->user->name }}</small>
+    </div>
+</li>
