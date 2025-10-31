507bcbd5cbc240c8d981e4e1d5aa3b0e04ad824d
gp-kato <kato@gp1000.jp>
2025-10-31 15:01:47 +0900

    [fat] チャンネルの接続やメッセージ送信などの処理をchat.bladeに移植

--- src/laravel-chat/resources/views/chat.blade.php
+++ src/laravel-chat/resources/views/chat.blade.php
@@ -118,9 +118,59 @@
 @endsection
 
 @section('js')  {{-- 「js」セクションにスクリプトを注入 --}}
-    <script>
+    <script type="module">
         window.App = window.App || {}; // グローバル名前空間の初期化
         window.App.user_id = {!! json_encode(auth()->id()) !!}; // ログインユーザーIDを格納
         const groupId = {{ $group->id }};
+
+        Echo.private(`group.${groupId}`).listen("MessageEvent", function (e) {
+            const div = document.getElementById("messages");
+            const html = e.html;
+            const noMessages = div.querySelector(".no-messages");
+            if (noMessages) {
+                noMessages.remove();
+            }
+
+            div.insertAdjacentHTML("beforeend", html);
+
+            const newMessage = div.lastElementChild;
+
+            if (e.user_id === window.App.user_id) {
+                newMessage.classList.add("justify-content-end");
+            }
+        });
+
+        document.getElementById("send").addEventListener("click", function () {
+            const message = document.getElementById("message").value;
+            if (message === "") return;
+            axios.post(`/groups/${groupId}/messages`, { content: message })
+
+            .then(() => {
+                document.getElementById("message").value = "";
+            })
+
+            .catch(error => {
+                if (!error.response) {
+                    console.error("ネットワークエラー:", error);
+                    alert("ネットワークエラーが発生しました。ネットワーク接続を確認してください。");
+                    return;
+                }
+
+                // ステータスコード別処理
+                const status = error.response.status;
+
+                // バリデーションエラー
+                if (status === 400 || status === 422) {
+                    const errors = error.response.data.errors;
+                    const messageError = errors?.content?.[1] || "正しい入力値で入力してください。";
+                    alert(messageError);
+                    return;
+                }
+
+                // その他のサーバーエラー
+                console.error("サーバーエラー:", error.response);
+                alert("サーバーエラーが発生しました。後でもう一度お試しください。");
+            });
+        });
     </script>
 @endsection
