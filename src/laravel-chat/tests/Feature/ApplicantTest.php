8f8f0253f1fe430b9cf0098795ae7941d0bac08b
gp-kato <kato@gp1000.jp>
2026-06-03 14:38:19 +0900

    [test] 申請者にも招待を送れることをテスト

--- src/laravel-chat/tests/Feature/ApplicantTest.php
+++ src/laravel-chat/tests/Feature/ApplicantTest.php
@@ -29,6 +29,15 @@ class ApplicantTest extends TestCase
         ]);
     }
 
+    private function adminGroup(User $user, Group $group): void
+    {
+        $group->users()->attach($user->id, [
+            'joined_at' => now(),
+            'left_at' => null,
+            'role' => 'admin',
+        ]);
+    }
+
     public function test_applicant_is_not_in_activemembers(): void
     {
         $this->actingAs($this->user);
@@ -39,6 +48,34 @@ class ApplicantTest extends TestCase
         $this->assertFalse(
             $activeMembers
         );
+    }
+
+    public function test_can_invite_applicant(): void
+    {
+        $this->actingAs($this->user);
+        $this->adminGroup($this->user, $this->group);
+
+        $inviteUser = User::factory()->create([
+            'email' => 'invitee@example.com',
+        ]);
+
+        $this->group->users()->attach($inviteUser->id, [
+            'role' => 'applicant',
+        ]);
+
+        $response = $this->post(
+            route('groups.invitations.invite', $this->group),
+            ['user_id' => $inviteUser->id,]
+        );
+
+        $response->assertSessionHasNoErrors();
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('invitations', [
+            'group_id'       => $this->group->id,
+            'inviter_id'     => $this->user->id,
+            'accepted_at'    => null,
+        ]);
     }
 
     public function test_applicant_is_not_in_activeusers(): void
